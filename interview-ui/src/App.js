1755fe2a8ebce3754c0c004f92434bfe5168e48a
Abram Simon <abram@abramsimon.com>
2018-03-26 16:25:26 -0400

    0004

--- interview-ui/src/App.js
+++ interview-ui/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Abram's React v0003</h1>
+          <h1 className="App-title">Welcome to Abram's React v0004</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
