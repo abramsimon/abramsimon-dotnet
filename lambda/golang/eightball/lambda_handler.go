541aa07d4a7fb0a58a34f3cc03fcd026b3ed129a
Abram Simon <abram@abramsimon.com>
2020-04-04 15:24:30 -0400

    Updated deps

--- lambda/golang/eightball/lambda_handler.go
+++ lambda/golang/eightball/lambda_handler.go
@@ -3,8 +3,14 @@ package main
 import (
 	"net/http"
 
+	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+type Outcome struct {
+	Text string `json:"_text"`
+}
 
 type Response struct {
 	StatusCode int               `json:"statusCode"`
@@ -13,13 +19,29 @@ type Response struct {
 }
 
 func HandleRequest() (Response, error) {
+	var err error
+
 	answer := "That's a great question! Unfortunately it requires a great answer that I don't currently have. Maybe ask something easier?"
 
-	req, _ := http.NewRequest("GET", "https://api.wit.ai/message?v=20190804&q=", nil)
+	req, err := http.NewRequest("GET", "https://api.wit.ai/message?v=20190804&q=", nil)
+	if err != nil {
+		return Response{}, err
+	}
 	req.Header.Add("Authorization", "Bearer H5QV4QBU4V7WDXGDFCU3IDO4JDMMAMUM")
 
 	client := &http.Client{}
-	resp, _ := client.Do(req)
+	resp, err := client.Do(req)
+	if err != nil {
+		return Response{}, err
+	}
+
+	outcomes := []Outcome{}
+
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&outcomes)
+	if err != nil {
+		return Response{}, err
+	}
 
 	return Response{
 		StatusCode: 200,
